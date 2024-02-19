ad47d4d61502a5f4a043e5a123b5d5596857e896
Arnau Rodríguez Pujals <a.rodriguez@ceroca.cat>
2024-02-19 09:13:28 +0100

    UI pantalla login

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/components/my_button.dart';
+import 'package:flutter_firebase_auth/components/my_textfield.dart';
+import 'package:flutter_firebase_auth/components/square_tile.dart';
+
+class LoginPage extends StatelessWidget {
+  LoginPage({super.key});
+
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+
+              // Logo.
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+
+              const SizedBox(
+                height: 50,
+              ),
+
+              // Welcome back message.
+              Text(
+                "Welcome back, you've been missed!",
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              // Username field.
+              MyTextField(
+                controller: usernameController,
+                hintText: "Username",
+                obscureText: false,
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              // Password field.
+              MyTextField(
+                controller: passwordController,
+                hintText: "Password",
+                obscureText: true,
+              ),
+
+              const SizedBox(
+                height: 10,
+              ),
+
+              // Forgot password?
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 25),
+                child: Row(
+                  // En una Row per posar-lo a la dreta.
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text("Forgot password?"),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 25,
+              ),
+
+              // Sign in button
+              MyButton(
+                onTap: signUserIn,
+              ),
+
+              const SizedBox(
+                height: 50,
+              ),
+
+              // or continue with
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 25,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                      ),
+                      child: Text(
+                        "Or continue with",
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SizedBox(
+                height: 50,
+              ),
+
+              // Google or Apple Sign in buttons.
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Google button.
+                  SquareTile(imagePath: "lib/images/google.png"),
+
+                  SizedBox(
+                    width: 25,
+                  ),
+
+                  // Apple button.
+                  SquareTile(imagePath: "lib/images/apple.png"),
+                ],
+              ),
+
+              const SizedBox(
+                height: 50,
+              ),
+
+              // Not a member? Register now.
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Not a member?"),
+                  SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    "Register now",
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
