e4f7d2b02222ecc745ea7486bac09236ea47e08e
Arnau Rodríguez Pujals <a.rodriguez@ceroca.cat>
2024-02-19 10:45:53 +0100

    Accedint a l'email de l'usuari

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final user = FirebaseAuth.instance.currentUser!;
+  // "!" indica que el valor no serà null.
+  //    No ho és perquè si ho fos, no ens hauria redirigit cap 
+  //    aquesta pàgina el auth_page.dart.
 
   void signUserOut() {
 
@@ -20,7 +25,13 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: Center(child: Text("Logged in!")),
+      body: Center(
+        child: Text(
+          "Logged in as: " + user.email!,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+      // Provar aquí de mostrar què passa si no hi posem el "!".
     );
   }
 }
