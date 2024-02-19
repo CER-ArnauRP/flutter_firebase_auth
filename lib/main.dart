4077fc87559333161896dc449ceed965768ead81
Arnau Rodríguez Pujals <a.rodriguez@ceroca.cat>
2024-02-19 10:12:10 +0100

    FirebaseAuth inclòs en el projecte

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/firebase_options.dart';
 import 'package:flutter_firebase_auth/pages/login_page.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 
